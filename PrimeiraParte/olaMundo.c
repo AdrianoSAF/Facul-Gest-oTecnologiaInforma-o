51ed63a7498bd64c370f4bd6015da5d964dbe88c
Adriano Dos Santos Araújo Filho <53800947+AdrianoSAF@users.noreply.github.com>
2024-11-19 16:43:48 +0000

    Aula: variáveis

---
+++ PrimeiraParte/olaMundo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int first_number = 1, sec_number = 2;
+    printf("Olá, mundo: %d", first_number + sec_number);
+
+
+return 0;
+
+}
---
+++ PrimeiraParte/variaveis.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 25, quantidade = 1;
+    float altura = 1.75;
+    double peso = 90.3;
+    char letra;
+    char nome;
+
+    nome = "Adriano";
+    letra = "a";
+}
