f333ed9497c0fa0d9088015ab233018dd5207f7b
Adriano Dos Santos Araújo Filho <53800947+AdrianoSAF@users.noreply.github.com>
2024-11-21 11:43:19 +0000

    Operadores relacionais (etapa Mestre)

---
+++ PrimeiraParte/Mestre/OperadoresRelacionais.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/*
+    Operadores Relacionais
+    > (maior que)
+    < (menor que)
+    >= (maior ou igual a)
+    <= (menor ou igual a)
+    == (igual a)
+    != (diferente de)
+*/
+
+int main(){
+    int a = 10, b = 20;
+
+    printf("a > b: %d\n", a > b);
+    printf("a < b: %d\n", a < b);
+    printf("a ==  b: %d\n", a == b);
+    printf("a != b: %d\n", a != b);
+
+    //Sem conversão explicita
+    int x = 5;
+    float y = 5.0;
+
+    printf("x == y: %d\n", x == y);
+    printf("x != y: %d\n", x != y);
+
+    //Com conversão explicita
+    //As veses será preciso
+    int num = 10;
+    float result = 10.0;
+
+    int comparison = (float)num == result;
+
+    printf("num == result: %d\n", comparison);
+    return 0;
+}
